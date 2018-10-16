16fcc883da8e8db59196ca119a8cf46a984809df
Jeferson Daniel <jeferson.daniel412@gmail.com>
2018-10-16 18:28:02 -0300

    Fixing kernel

--- phoopy/proxy_command.py
+++ phoopy/proxy_command.py
@@ -4,7 +4,6 @@ from cleo import Command
 
 class ProxyCommand(Command):
     def __init__(self, command_class, service_key, container):
-        self.__doc__ = re.sub(r'service=.*', '', command_class.__doc__)
         super(ProxyCommand, self).__init__()
         self.__service_key = service_key
         self.__container = container
