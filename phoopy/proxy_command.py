54861d4f7bb642e9ca8054ab8abf51a005ba42e5
Rafael Reis <reisraff@gmail.com>
2018-10-16 18:44:17 -0300

    fixing command init

--- phoopy/proxy_command.py
+++ phoopy/proxy_command.py
@@ -1,9 +1,9 @@
-import re
 from cleo import Command
 
 
 class ProxyCommand(Command):
     def __init__(self, command_class, service_key, container):
+        self.__doc__ = command_class.__doc__
         super(ProxyCommand, self).__init__()
         self.__service_key = service_key
         self.__container = container
