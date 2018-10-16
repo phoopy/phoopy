8b5c114337b31c4e2e7f28befa5d45688c200e7f
Rafael Reis <reisraff@gmail.com>
2018-10-16 17:31:25 -0300

    fixing importing

--- phoopy/__init__.py
+++ phoopy/__init__.py
@@ -2,7 +2,6 @@ from .abstract_command import AbstractCommand  # noqa
 from .application import Application  # noqa
 from .bundle import Bundle  # noqa
 from .container import Container  # noqa
-from .helper import Helper  # noqa
 from .kernel import Kernel  # noqa
 from .proxy_command import ProxyCommand  # noqa
 from .yaml_parser import YamlParser  # noqa
